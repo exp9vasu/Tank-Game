d0fc96675810555e7357359a0725c6e87ca37207
DESKTOP-DOGBSBL\hp <vasupandey777@gmail.com>
2022-07-17 18:13:00 +0530

    added Tankspawner

---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    public GameObject TankPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(TankPrefab, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
